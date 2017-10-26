958e97a4c6cb6ac1966e6e971563d9248421e4ca
sanket ketkar <sketkar03@gmail.com>
2017-10-26 16:19:16 +0530

    test

---
+++ src/scripts/utilities/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from '../containers/app.jsx';
+import '../../styles/app.scss';
+
+render(<App/>, document.getElementById('app'));
