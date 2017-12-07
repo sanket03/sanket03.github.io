deefbd45167d62823eb1854b0ed090e35add0dd4
sanket ketkar <sketkar03@gmail.com>
2017-12-07 14:42:15 +0530

    Initial commit

--- src/scripts/components/routes.jsx
+++ src/scripts/components/routes.jsx
@@ -16,7 +16,7 @@ let Routes = () => {
             <Router>
                 <Switch>
                     <Route exact path = '/src/' render = {() => (<App/>)}/>
-                    <Route path = {`${process.env.PUBLIC_URL}/:casestudy`} render = {(props) => (<CaseStudy {...props}/>)}/>
+                    <Route path = {`/src/:casestudy`} render = {(props) => (<CaseStudy {...props}/>)}/>
                 </Switch>
             </Router>
             <Footer/>
