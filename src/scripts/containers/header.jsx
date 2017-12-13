65e48b0140fd6eb4783d933c0336f76a64264710
v-saket <v-saketk@microsoft.com>
2017-12-13 22:10:40 +0530

    test

--- src/scripts/containers/header.jsx
+++ src/scripts/containers/header.jsx
@@ -68,7 +68,7 @@ export default class Header extends React.Component {
                     </li>
 
                     <li>
-                        <ScrollLink to = 'contact-container' smooth = {true} duration={10000} delay = {200}>
+                        <ScrollLink to = 'contact-container' smooth = {true} duration={6000}>
                             Contact
                         </ScrollLink>
                     </li>
