e6c14959fbabfa7c637f785a862c92006d956ff6
sanket ketkar <sketkar03@gmail.com>
2018-10-25 02:23:25 +0530

    added new project

---
+++ precache-manifest.1fdcbe6528400df07aec1fcc5eb063f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "252c00f4d019c143f94b",
+    "url": "/static/js/main.252c00f4.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "252c00f4d019c143f94b",
+    "url": "/static/css/main.3d0344c8.chunk.css"
+  },
+  {
+    "revision": "72e2603ffec5da8072ec3360558a6a6a",
+    "url": "/index.html"
+  }
+];
--- src/scripts/utilities/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Routes from '../components/routes.jsx';
-import '../../styles/app.scss';
-
-render(<Routes/>, document.getElementById('app'));
