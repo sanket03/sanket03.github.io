104bc937809713bd466c514266768b8bea48fd6f
Sanket Ketkar <v-saketk@microsoft.com>
2018-11-26 11:41:19 +0530

    showcase new functionalities

---
+++ precache-manifest.9a2e6333f7e6c54fcc41b2f1fdb495da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1d3a971bc45b24cad38",
+    "url": "/static/js/main.c1d3a971.chunk.js"
+  },
+  {
+    "revision": "28ad37fbe3ddc8ce71c9",
+    "url": "/static/js/1.28ad37fb.chunk.js"
+  },
+  {
+    "revision": "c1d3a971bc45b24cad38",
+    "url": "/static/css/main.9b780c63.chunk.css"
+  },
+  {
+    "revision": "7e7a389f69e0f6a2d9691664a1a4cb36",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b610a47f9502688ca69f1ece83ab4dff.js"
+  "/precache-manifest.9a2e6333f7e6c54fcc41b2f1fdb495da.js"
 );
 
 workbox.clientsClaim();
